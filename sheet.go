fd2351d822f2b5c8f799566ecc5bcb3f726c5080
uenoryo <r.r.ag0611@outlook.jp>
2019-02-10 18:56:44 +0900

    mapにできるように.

--- sheet.go
+++ sheet.go
@@ -2,9 +2,10 @@ package ssfet
 
 // Sheet (､´･ω･)▄︻┻┳═一
 type Sheet struct {
-    ID    string
-    Title string
-    Rows  [][]string
+    ID       string
+    Title    string
+    Rows     [][]string
+    _mapRows []map[string]string
 }
 
 // NewSheet (､´･ω･)▄︻┻┳═一
@@ -14,4 +15,27 @@ func NewSheet(sheetID, title string, rows [][]string) *Sheet {
         Title: title,
         Rows:  rows,
     }
+}
+
+func (sheet *Sheet) toMapRows() []map[string]string {
+    if len(sheet._mapRows) != 0 {
+        return sheet._mapRows
+    }
+
+    if len(sheet.Rows) == 0 {
+        return make([]map[string]string)
+    }
+
+    var (
+        columns = sheet.Rows[0]
+        mapRows = make([]map[string]string)
+    )
+    for i, row := range sheet.Rows[1:] {
+        rowMap := make(map[string]string)
+        for i, column := range columns {
+            rowMap[column] = row[i]
+        }
+        mapRows = append(mapRows, rowMap)
+    }
+    return mapRows
 }
